98ba26b2af79d0e5986dfe793178ebba16b8e1b4
FleaFX <wouter.naessens@outlook.com>
2015-02-13 16:13:38 +0100

    Created AmsPacketAdapter

---
+++ src/Ads.Tests/AmsPacketAdapterTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace Ads {
+  [TestFixture]
+  public class AmsPacketAdapterTests {
+    IAdapter<byte[], AmsPacket> _adapter;
+    AmsPacket _result;
+    
+    [TestFixtureSetUp]
+    public void FixtureSetUp() {
+      _adapter = new AmsPacketAdapter();
+      _result = _adapter.Adapt(new byte[] {
+        192, 168, 10, 14, 1, 1, 33, 3,  // 8 bytes: target address (net id + port)
+        127, 0, 0, 1, 1, 1, 217, 4,     // 8 bytes: source address (net id + port)
+        0, 1,                           // 2 bytes: command id
+        0, 5,                           // 2 bytes: state flags
+        0, 0, 0, 24,                    // 4 bytes: length
+        0, 0, 0, 24,                    // 4 bytes: error code
+        0, 0, 0, 25,                    // 4 bytes: invoke id
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 // data: number of bytes as specified in length field
+      });
+    }
+
+    [Test]
+    public void AdaptCreatesExpectedAmsPacket() {
+      Assert.That(_result, Is.EqualTo(
+        new AmsPacket(
+          AmsAddress.Parse("127.0.0.1.1.1:1241"),
+          AmsAddress.Parse("192.168.10.14.1.1:801"),
+          CommandId.AdsReadDeviceInfo,
+          StateFlags.AdsCommand | StateFlags.Response,
+          new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24 })));
+    }
+  }
+}
---
+++ src/Ads/AmsPacketAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Ads {
+  public class AmsPacketAdapter : IAdapter<byte[], AmsPacket> {
+    /// <summary>
+    /// Adapts a <see><cref>byte[]</cref></see> to produce a <see cref="AmsPacket"/>.
+    /// </summary>
+    /// <param name="subject">The object being adapter.</param>
+    /// <returns>A <see cref="AmsPacket"/>.</returns>
+    public AmsPacket Adapt(byte[] subject) {
+      // TODO: find out if bytes are big endian or little endian
+      var payloadLength = BitConverter.ToInt32(subject.Skip(20).Take(4).Reverse().ToArray(), 0);
+      return new AmsPacket(
+        new AmsAddress(new AmsNetId(subject.Skip(8).Take(6).ToArray()), new AmsPort(BitConverter.ToUInt16(subject.Skip(14).Take(2).ToArray(), 0))),
+        new AmsAddress(new AmsNetId(subject.Take(6).ToArray()), new AmsPort(BitConverter.ToUInt16(subject.Skip(6).Take(2).ToArray(), 0))),
+        (CommandId)BitConverter.ToUInt16(subject.Skip(16).Take(2).Reverse().ToArray(), 0),
+        (StateFlags)BitConverter.ToUInt16(subject.Skip(18).Take(2).Reverse().ToArray(), 0),
+        subject.Skip(32).Take(payloadLength).ToArray());
+    }
+  }
+}
